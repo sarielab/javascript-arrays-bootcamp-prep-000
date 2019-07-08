4c8f8b41b6f03501acb1f8db3b87e2e2637d9086
Poppy Puspa Sari <poppy.puspasari@yahoo.co.id>
2019-07-08 16:18:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,13 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 var addElementToBeginningOfArray = (arr, el) => [el, ...arr]
-var destructivelyAddElementToBeginningOfArray = (arr, el) => arr.unshift(el)
+var destructivelyAddElementToBeginningOfArray = (arr, el) => {
+  return arr
+}
 var addElementToEndOfArray = (arr, el) => [...arr, el]
-var destructivelyAddElementToEndOfArray = (arr, el) => arr.push(el)
+var destructivelyAddElementToEndOfArray = (arr, el) => {
+  arr.push(el)
+  return arr
+}
 var accessElementInArray = (arr, el) => arr[el]
 var destructivelyRemoveElementFromBeginningOfArray = (arr) => arr.shift()
 var removeElementFromBeginningOfArray = (arr) => arr.slice(1)
